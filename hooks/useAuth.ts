51cc27dae43038b7ba5a323f0dca95fa7ae2d8cb
Mohammed Elghanam <144786058+MohammedElghanam@users.noreply.github.com>
2025-01-14 20:43:42 +0100

    feat: adc the code of authentication

---
+++ hooks/useAuth.ts
@@ -0,0 +1,32 @@
+// hooks/useAuth.ts
+import { useState, useEffect } from "react";
+import { onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from "firebase/auth";
+import { auth } from "../constants/firebaseConfig";
+
+export const useAuth = () => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const signIn = (email: string, password: string) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const signUp = (email: string, password: string) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const logOut = () => {
+    return signOut(auth);
+  };
+
+  return { user, loading, signIn, signUp, logOut };
+};
